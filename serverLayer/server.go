8a3c18f53cab9bb39c626df2b8706afdd16b0dab
heinirich <smithheinrich@gmail.com>
2025-05-31 08:24:47 +0300

    Fixes

--- serverLayer/server.go
+++ serverLayer/server.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"Go-WebSockets/socket"
 	"golang.org/x/net/websocket"
 	"net/http"
 )
 
 func main() {
+
+	config := socket.NewConfig()
 	// Define new Server mux
 	muxServer := http.NewServeMux()
 
-	muxServer.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+	muxServer.Handle("/", websocket.Handler(func(wsConnection *websocket.Conn) {
+		webSocketHandler(wsConnection, config)
 	}))
 	server := http.Server{
 		Addr:    "127.0.0.1:8085",
@@ -19,5 +23,21 @@ func main() {
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
+	}
+}
+
+func webSocketHandler(c *websocket.Conn, config *socket.Config) {
+	go config.RunSocket()
+
+	config.ClientRegisterChan <- c
+
+	for {
+		var message socket.Message
+		err := websocket.JSON.Receive(c, &message)
+		if err != nil {
+			config.UnregisterClient <- c
+			continue
+		}
+		config.MessageData <- message
 	}
 }
