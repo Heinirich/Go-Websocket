6e974294949e496d2478b4e5f256aebc890b20b3
heinirich <smithheinrich@gmail.com>
2025-05-31 08:16:12 +0300

    Defined Server Layer and Main function

--- serverLayer/server.go
+++ serverLayer/server.go
@@ -1,1 +1,23 @@
-package serverLayer
+package main
+
+import (
+	"golang.org/x/net/websocket"
+	"net/http"
+)
+
+func main() {
+	// Define new Server mux
+	muxServer := http.NewServeMux()
+
+	muxServer.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+	}))
+	server := http.Server{
+		Addr:    "127.0.0.1:8085",
+		Handler: muxServer,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
